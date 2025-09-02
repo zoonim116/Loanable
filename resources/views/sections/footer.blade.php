538f069c2babcf25b48e8dfc629cb4ffb7cce7f9
Maxim Nepomniashchiy <maximko91@gmail.com>
2025-09-02 11:24:30 +0300

    Fixes and improvements

--- resources/views/sections/footer.blade.php
+++ resources/views/sections/footer.blade.php
@@ -72,8 +72,7 @@
     <div class="modal micromodal-slide" id="contact-form" aria-hidden="true">
       <div class="modal__overlay" tabindex="-1" data-micromodal-close>
         <div class="modal__container" role="dialog" aria-modal="true" aria-labelledby="contact-form-title" >
-          <header class="modal__header">
-            <h2 class="modal__title" id="contact-form-title">{{ $contact_form_title }}</h2>
+          <header class="modal__header flex justify-end">
             <button class="modal__close" aria-label="Close modal" data-micromodal-close></button>
           </header>
           <div class="modal__content" id="contact-form-content">
