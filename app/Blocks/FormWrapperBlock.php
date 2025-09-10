1773bba3ba391b1d98467ef89f0df4feae39f6ca
Maxim Nepomniashchiy <maximko91@gmail.com>
2025-09-10 16:28:51 +0300

    Change colors

--- app/Blocks/FormWrapperBlock.php
+++ app/Blocks/FormWrapperBlock.php
@@ -148,6 +148,6 @@ class FormWrapperBlock extends Block
 
     public function getClasses(): string
     {
-        return 'wp-block-form-wrapper-block  py-10 lg:py-20 bg-blue-600';
+        return 'wp-block-form-wrapper-block  py-10 lg:py-20 bg-blue-300';
     }
 }
