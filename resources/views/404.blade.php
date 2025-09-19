3e54ce6f48d9c66142a4b67ec0c5f1df74c5c31c
Maxim Nepomniashchiy <maximko91@gmail.com>
2025-09-19 16:26:48 +0300

    Fixes

--- resources/views/404.blade.php
+++ resources/views/404.blade.php
@@ -4,6 +4,7 @@
   @include('partials.page-header')
 
   @if (! have_posts())
+    <div class="container container-lg pt-8 pb-8 text-block"></div>
     <x-alert type="warning">
       {!! __('Sorry, but the page you are trying to view does not exist.', 'sage') !!}
     </x-alert>
