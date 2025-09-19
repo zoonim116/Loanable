68279e15b99c1b87673f1b1325f715e754d7c756
Maxim Nepomniashchiy <maximko91@gmail.com>
2025-09-19 16:28:29 +0300

    Fix 404

--- resources/views/404.blade.php
+++ resources/views/404.blade.php
@@ -4,11 +4,10 @@
   @include('partials.page-header')
 
   @if (! have_posts())
-    <div class="container container-lg pt-8 pb-8 text-block"></div>
-    <x-alert type="warning">
-      {!! __('Sorry, but the page you are trying to view does not exist.', 'sage') !!}
-    </x-alert>
-
-    {!! get_search_form(false) !!}
+    <div class="container container-lg pt-8 pb-8 text-block">
+        <x-alert type="warning">
+          {!! __('Sorry, but the page you are trying to view does not exist.', 'sage') !!}
+        </x-alert>
+      </div>
   @endif
 @endsection
