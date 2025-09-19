55df431646037eb63751a1dc10edff8aabc1faf5
Maxim Nepomniashchiy <maximko91@gmail.com>
2025-09-19 15:05:51 +0300

    Fixes

--- resources/views/template-custom.blade.php
+++ resources/views/template-custom.blade.php
@@ -5,8 +5,10 @@
 @extends('layouts.app')
 
 @section('content')
-  @while(have_posts()) @php(the_post())
-    @include('partials.page-header')
-    @include('partials.content-page')
-  @endwhile
+  <div class="container container-lg pt-8 pb-8 text-block">
+    @while(have_posts()) @php(the_post())
+      @include('partials.page-header')
+      @include('partials.content-page')
+    @endwhile
+  </div>
 @endsection
