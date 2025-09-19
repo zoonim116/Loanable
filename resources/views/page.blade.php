dfddcd0cb843f77a49b5ec3a2e76421b4b496787
Maxim Nepomniashchiy <maximko91@gmail.com>
2025-09-19 11:56:07 +0300

    Remove container

--- resources/views/page.blade.php
+++ resources/views/page.blade.php
@@ -1,10 +1,8 @@
 @extends('layouts.app')
 
 @section('content')
-  <div class="container container-lg pt-8 pb-8">
     @while(have_posts()) @php(the_post())
       @include('partials.page-header')
       @includeFirst(['partials.content-page', 'partials.content'])
     @endwhile
-  </div>
 @endsection
